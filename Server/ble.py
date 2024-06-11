2a7573d11a118b132f1146c91bc41dc330de9595
newwin01 <newwin0198@handong.ac.kr>
2024-06-11 22:13:43 +0900

    feat: update connection function

--- Server/ble.py
+++ Server/ble.py
@@ -1,57 +1,42 @@
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services import Service
+from adafruit_ble.characteristics import Characteristic
+from adafruit_ble.characteristics.int import Uint8Characteristic
+from adafruit_ble.characteristics.stream import StreamIn
+import adafruit_ble
 from adafruit_ble.services.nordic import UARTService
-import re
 
-# ID = ['x20', 'x43', 'x6f', 'x6e', 'x6e', 'x65', 'x63', 'x74'] Using NRF Desktop BLE
-ID = ['x75', 'x06', 'x09', 'x6e', 'x52', 'x46', 'x35', 'x78'] #Build using NRF Connect
+# Define device IDs
+ID1 = "2BE99CA4-1DAA-3A26-57F4-4D046484CDDF"
+ID2 = "1EE3DDA6-D71B-D36B-F8D9-80A276B9D891"
+
 device = None
-message = None
-buffer = 64
+count = 0
 
+# Initialize BLE radio
 radio = BLERadio()
-print("scanning")
+print("Scanning for devices...")
 found = set()
+
+# Scan for devices
 for entry in radio.start_scan(timeout=60, minimum_rssi=-80):
     addr = entry.address
-    if addr not in found:
-        print(entry)
-        s = repr(entry)
-        s = re.findall(r'"(.+?)"',s)
-        s = s[0]
-        s = s[-31:].split("\\")
-        print(s)
 
-        if s == ID:
-            device = radio.connect(entry)
-            print("device connected!") 
-            break
+    if addr not in found:
+        print(entry.complete_name)
+
+        if addr.string == ID1 or addr.string == ID2:
+            found.add(addr)
+            if count == 0:
+                device = radio.connect(entry)
+                print("Device connected!")
+                count += 1
+                # break
+            else:
+                device = radio.connect(entry)
+                print("Device connected!")
+                break
 
 radio.stop_scan()
 
-while device and device.connected:
-    print("Connected to device. Checking for UARTService...")
-
-    try:
-        # Check if the UARTService is available in the connected device
-        if UARTService in device:
-            uart_service = device[UARTService]
-            print("UARTService found!")
-            while device.connected:
-                try:
-                    # Read data from the UART service
-                    data = uart_service.read(buffer_size)
-                    if data:
-                        message = data.decode('utf-8')
-                        print("Received message:", message)
-                except OSError as e:
-                    print("Read error:", e)
-                    device.disconnect()
-                    device = None
-                    break
-        else:
-            print("UARTService not found in the connected device.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-else:
-    print("Failed to connect to the device.")
