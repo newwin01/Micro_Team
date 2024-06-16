d82670bc8253aa0c45ea5f9f65d9c2067d4f6104
Han0524 <hsh21900783@handong.ac.kr>
2024-06-16 17:17:08 +0900

    button1,2,3,4 change

--- Server/ble.py
+++ Server/ble.py
@@ -5,8 +5,13 @@ import subprocess
 import threading
 import time
 
+# Global variable to store the current process
+current_process = None
+
 # Function to read from Bluetooth device and send to subprocess
-def read_from_device_and_send(device, process):
+def read_from_device_and_send(device):
+    global current_process
+
     while device and device.connected:
         # Read data from the Bluetooth device
         data = device[UARTService].read(20)  # Adjust buffer size as needed
@@ -15,47 +20,61 @@ def read_from_device_and_send(device, process):
             data = data.decode('utf-8').strip()
 
             # Send the appropriate commands to the subprocess
-            if data == "Button1":
-
-                process.stdin.write(b"END\n")
-                process.stdin.flush()
-                process.stdin.write(b"END\n")
-                process.stdin.flush()
+            if data == "Button4":
+                current_process.stdin.write(b"END\n")
+                current_process.stdin.flush()
+                current_process.stdin.write(b"END\n")
+                current_process.stdin.flush()
                 break
 
             elif data.find("Up") != -1:
-                process.stdin.write(b"UP\n")
-                
-                process.stdin.flush()
+                current_process.stdin.write(b"UP\n")
+                current_process.stdin.flush()
 
             elif data.find("Down") != -1:
-                process.stdin.write(b"DOWN\n")
-               
-                process.stdin.flush()
+                current_process.stdin.write(b"DOWN\n")
+                current_process.stdin.flush()
 
             elif data.find("Left") != -1:
-                process.stdin.write(b"LEFT\n")
-                process.stdin.flush()
+                current_process.stdin.write(b"LEFT\n")
+                current_process.stdin.flush()
 
             elif data.find("Right") != -1:
-                process.stdin.write(b"RIGHT\n")
-                process.stdin.flush()
-            
+                current_process.stdin.write(b"RIGHT\n")
+                current_process.stdin.flush()
+
+            elif data == "Button2":
+                start_new_game("blicker.py")
+
+            elif data == "Button3":
+                start_new_game("pong.py")
+
+            elif data == "Button1":
+                start_new_game("snake.py")
 
             print(data)
 
+def start_new_game(game_script):
+    global current_process
+
+    if current_process:
+        current_process.stdin.write(b"END\n")
+        current_process.stdin.flush()
+        current_process.terminate()
+        current_process.wait()
+
+    current_process = subprocess.Popen(['python3', game_script], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 # Function to run the subprocess
 def run_subprocess():
-    # Start the subprocess
-    process = subprocess.Popen(['python3', 'snake.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    global current_process
+
+    # Start the initial game
+    start_new_game('snake.py')
 
     # Start thread to read from Bluetooth device and send to subprocess
-    read_thread = threading.Thread(target=read_from_device_and_send, args=(device, process))
+    read_thread = threading.Thread(target=read_from_device_and_send, args=(device,))
     read_thread.start()
-
-    stdout = process.poll()
-
-    print(stdout)
 
     # Wait for the read thread to finish
     read_thread.join()
@@ -74,7 +93,7 @@ for entry in radio.start_scan(timeout=1200, minimum_rssi=-80):
         print(entry.complete_name)
 
         # Replace with your device IDs
-        if entry.complete_name in ["Jang"]:
+        if entry.complete_name in ["Han"]:
             found.add(addr)
             device = radio.connect(entry)
             print("Device connected!")
@@ -87,12 +106,8 @@ if device:
     subprocess_thread = threading.Thread(target=run_subprocess)
     subprocess_thread.start()
 
-   
     while device.connected:
-  
-
-        pass  
-
+        pass
 
     subprocess_thread.join()
 
