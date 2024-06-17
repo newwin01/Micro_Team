f8eadeb2da6727e46f3909ca31ba07009b5af2d5
Han0524 <hsh21900783@handong.ac.kr>
2024-06-17 18:56:55 +0900

    Button4 success

--- Server/ble.py
+++ Server/ble.py
@@ -7,10 +7,11 @@ import time
 
 # Global variable to store the current process
 current_process = None
+terminate_signal = False
 
 # Function to read from Bluetooth device and send to subprocess
 def read_from_device_and_send(device):
-    global current_process
+    global current_process, terminate_signal
 
     while device and device.connected:
         # Read data from the Bluetooth device
@@ -18,41 +19,40 @@ def read_from_device_and_send(device):
 
         if data:
             data = data.decode('utf-8').strip()
+            print(f"Received data: {data}")
 
             # Send the appropriate commands to the subprocess
-            if data.find("Button4") != -1:
-                current_process.stdin.write(b"END\n")
-                current_process.stdin.flush()
-                current_process.stdin.write(b"END\n")
-                current_process.stdin.flush()
-                break
+            if data == "Button4":
+                if current_process:
+                    current_process.stdin.write(b"END\n")
+                    current_process.stdin.flush()
+                    terminate_signal = True
+                    break
 
-            elif data.find("Up") != -1:
+            elif data == "Up":
                 current_process.stdin.write(b"UP\n")
                 current_process.stdin.flush()
 
-            elif data.find("Down") != -1:
+            elif data == "Down":
                 current_process.stdin.write(b"DOWN\n")
                 current_process.stdin.flush()
 
-            elif data.find("Left") != -1:
+            elif data == "Left":
                 current_process.stdin.write(b"LEFT\n")
                 current_process.stdin.flush()
 
-            elif data.find("Right") != -1:
+            elif data == "Right":
                 current_process.stdin.write(b"RIGHT\n")
                 current_process.stdin.flush()
 
-            elif data.find("Button2") != -1:
+            elif data == "Button2":
                 start_new_game("blicker.py")
 
-            elif data.find("Button3") != -1:
+            elif data == "Button3":
                 start_new_game("pong.py")
 
-            elif data.find("Button1") != -1:
+            elif data == "Button1":
                 start_new_game("snake.py")
-
-            print(data)
 
 def start_new_game(game_script):
     global current_process
@@ -67,7 +67,7 @@ def start_new_game(game_script):
 
 # Function to run the subprocess
 def run_subprocess():
-    global current_process
+    global current_process, terminate_signal
 
     # Start the initial game
     start_new_game('snake.py')
@@ -75,6 +75,13 @@ def run_subprocess():
     # Start thread to read from Bluetooth device and send to subprocess
     read_thread = threading.Thread(target=read_from_device_and_send, args=(device,))
     read_thread.start()
+
+    while not terminate_signal:
+        time.sleep(0.1)
+
+    if current_process:
+        current_process.terminate()
+        current_process.wait()
 
     # Wait for the read thread to finish
     read_thread.join()
@@ -93,7 +100,7 @@ for entry in radio.start_scan(timeout=1200, minimum_rssi=-80):
         print(entry.complete_name)
 
         # Replace with your device IDs
-        if entry.complete_name in ["Jang"]:
+        if entry.complete_name in ["Han"]:
             found.add(addr)
             device = radio.connect(entry)
             print("Device connected!")
